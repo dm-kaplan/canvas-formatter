b9e05f4e9fef1b656359256be8a97087ed814869
dageekyman <YOUR_EMAIL@domain.com>
2025-11-13 12:14:05 -0500

    Fix Assessment Overview template footer structure

--- lib/formatters/index.ts
+++ lib/formatters/index.ts
@@ -1733,12 +1733,8 @@ function formatWFUAssessmentOverview(content: string, context: TemplateContext =
         </div>
     </div>
     <div class="grid-row">
-  <div class="col-xs-12 WFU-footer">
-          <div class="grid-row">
-            <div class="col-xs-12">
-                <footer class="WFU-footer">This material is owned by Wake Forest University and is protected by U.S. copyright laws. All Rights Reserved.</footer>
-            </div>
-          </div>
+        <div class="col-xs-12">
+            <footer class="WFU-footer">This material is owned by Wake Forest University and is protected by U.S. copyright laws. All Rights Reserved.</footer>
         </div>
     </div>
 </div>`;
